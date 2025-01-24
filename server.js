b9be7a9e90dc2731ae3bfd4d7461817585dd5418
Hamzathul Favas E <ehamzathulfavas@gmail.com>
2025-01-24 15:48:37 +0530

    fix: cors

--- server.js
+++ server.js
@@ -4,20 +4,24 @@ import cors from "cors";
 import morgan from "morgan";
 
 import connectDB from "./config/db.js";
-import invoiceRoutes from './routes/invoiceRoute.js'
+import invoiceRoutes from "./routes/invoiceRoute.js";
 
 dotenv.config();
 connectDB();
 
 const app = express();
 
-app.use(cors());
+// app.use(cors());
+app.use(
+  cors({
+    origin: "https://finifi-lovat.vercel.app",
+  })
+);
 app.use(express.json());
 
 //remove it from production
-app.use(morgan("dev"));
+// app.use(morgan("dev"));
 
 app.use("/api/invoices", invoiceRoutes);
-
 
 app.listen(5000, () => console.log("Server running on http://localhost:5000"));
